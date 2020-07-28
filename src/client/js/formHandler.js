f191eebec5db35162c99a046ac9107005cd9eadc
Megs <meghan.g.dunham@gmail.com>
2020-07-28 18:01:36 -0400

    Finalize website design

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,15 +2,12 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let result = document.getElementById('result');
+    result.classList.add('visible');
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    // Display error message
+    let error = document.getElementById('error-message');
+    error.classList.add('visible');
 }
 
 export { handleSubmit }
