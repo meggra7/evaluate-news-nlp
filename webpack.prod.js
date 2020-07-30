18f0879d25bcafe569f6243f730cf8f5798cdfbd
Megs <meghan.g.dunham@gmail.com>
2020-07-29 21:19:35 -0400

    Add optimization for production build config

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,18 @@ const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    optimization: {
+        minimizer: [
+            new TerserPlugin({}),
+            new OptimizeCSSAssetsPlugin({}),
+        ],
+    },
     module: {
         rules: [
             {
